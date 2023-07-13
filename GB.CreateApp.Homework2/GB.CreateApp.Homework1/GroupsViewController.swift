7ffe5cc67070149d88028681a93d389d9d813cbd
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2023-07-13 18:27:35 +0500

    Add files via upload

---
+++ GB.CreateApp.Homework2/GB.CreateApp.Homework1/GroupsViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+final class GroupsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Groups"
+        view.backgroundColor = .white
+        tableView.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .white
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt IndexPath: IndexPath) -> UITableViewCell{
+        GroupCell()
+    }
+}
